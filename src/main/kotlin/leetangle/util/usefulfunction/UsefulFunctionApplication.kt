c00bbf9616d8fe2f55c4c336d60870cfdaa9c72e
sm <smlee@danal.co.kr>
2024-05-25 17:51:50 +0900

    initial commit

---
+++ src/main/kotlin/leetangle/util/usefulfunction/UsefulFunctionApplication.kt
@@ -0,0 +1,11 @@
+package leetangle.util.usefulfunction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UsefulFunctionApplication
+
+fun main(args: Array<String>) {
+    runApplication<UsefulFunctionApplication>(*args)
+}
